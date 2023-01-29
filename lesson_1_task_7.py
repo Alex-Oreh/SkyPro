1add8235c60a95b21deb6f54d96a4c4f226c743c
Alex-Oreh <112081365+Alex-Oreh@users.noreply.github.com>
2023-01-29 15:13:30 +0300

    1-й урок

---
+++ lesson_1_task_7.py
@@ -0,0 +1,40 @@
+def funcA():
+    print(1 , end = '')
+
+def funcB():
+    print(2, end = '')
+
+def funcC():
+    print(3, end = '')
+
+def funcD():
+    print(4, end = '')
+
+def funcE():
+    print(5, end = '')
+
+def funcF():
+    print(6, end = '')
+
+def funcG():
+    print(7, end = '')
+
+def funcH():
+    print(8, end = '')
+    
+def funcI():
+    print(9, end = '')
+
+def funcX():
+    print(0, end = '')
+funcH()
+funcH()
+funcX()
+funcX() 
+funcE()
+funcE()
+funcE()
+funcC()
+funcE()
+funcC()
+funcE()
---
+++ lesson_1_task_8.py
@@ -0,0 +1,5 @@
+def func():
+    a = input()
+    print(a, end="")
+for i in range(11):
+    func()
